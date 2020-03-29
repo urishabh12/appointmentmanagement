1f92a60dbdebdc0e387008005d25cb6b3de86752
Rishabh Upadhayay <urishabh42@gmail.com>
2020-03-29 22:27:42 +0530

    beta

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import os
 import sys
-from flask import Flask, send_from_directory, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from flask import Flask, send_from_directory, render_template, request, redirect, url_for
 import pymongo
 import time
+import datetime
+from joblib import load
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["medicalappointment"]
 app = Flask(__name__)
+svm = load('appointmentmanagement.joblib') 
 
 def add_time(xtime, shr, smin):
     smin+=xtime
     if smin>60:
         shr+=1
         smin-=60
-    return str(shr)+":"+str(smin)
+    return str(shr).zfill(2)+":"+str(smin)
 
 def cmp_time(hr, minn, ehr, emin):
     if hr<ehr:
@@ -23,6 +26,24 @@ def cmp_time(hr, minn, ehr, emin):
         return True
     else:
         return False
+
+def getDate():
+    x = datetime.datetime.now()
+    return (x.year, x.month, x.day)
+
+def checkDate(appdate, date):
+    year=int(appdate[:4])
+    month=int(appdate[5:7])
+    day=int(appdate[8:])
+    if year>date[0]:
+        return True
+    elif year==date[0] and month>date[1]:
+        return True
+    elif year==date[0] and month==date[1] and day>=date[2]:
+        return True
+    else:
+        return False
+
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -60,54 +81,81 @@ def home(username, typeof):
 
 @app.route("/dhome/<string:username>/<string:typeof>", methods=['GET', 'POST'])
 def dhome(username, typeof):
+    mycoltwo = mydb["booked"]
+    appointments=[]
+    date=getDate()
+    for x in mycoltwo.find():
+        cdate=x["date"]
+        if x["dusername"]==username and checkDate(cdate, date):
+            appointments.append((x["pusername"], x["from"], x["to"], x["date"]))
     if request.method == "POST":
         mycol = mydb["avappoint"]
         fromm = request.form.get('from')
         to = request.form.get('to')
         date = request.form.get('date')
-            data = {'username': username, 'date':date, 'from':fromm, 'to':to, myappointments:{}, total:0}
+        data = {'username': username, 'date':date, 'from':fromm, 'to':to, "total":0, "ltime":fromm}
         x=mycol.insert_one(data)
-        return render_template('dhome.html')
-    return render_template('dhome.html')
+        return render_template('dhome.html', appointment=appointments)
+    return render_template('dhome.html', appointment=appointments)
 
 @app.route("/phome/<string:username>/<string:typeof>", methods=['GET', 'POST'])
 def phome(username, typeof):
     mycol = mydb["users"]
+    mycoltwo = mydb["booked"]
     doctors=[]
+    appointments=[]
+    date=getDate()
     for x in mycol.find():
         if x['typeof']=="doctor":
             doctors.append((x['name'], x['username']))
-    return render_template('phome.html', doctors=doctors, patient=username)
+    for x in mycoltwo.find():
+        cdate=x["date"]
+        if x["pusername"]==username and checkDate(cdate, date):
+            appointments.append((x["dusername"], x["from"], x["to"], x["date"]))
+    if len(appointments)==0:
+        return render_template('phome.html', doctors=doctors, patient=username, appointment="No Appointments available")
+    return render_template('phome.html', doctors=doctors, patient=username, appointment=appointments)
 
 @app.route("/appointment/<string:username>/<string:patient>", methods=['GET', 'POST'])
 def appointment(username, patient):
     mycol = mydb['avappoint']
     dates=[]
+    #get all available appointments
     for x in mycol.find():
         if x['username'] == username:
             dates.append(x['date'])
+    #for booking appointments        
     if request.method == "POST":
         date = request.form.get('dates')
-        symptom = request.form.get('symptoms')
-        myap = {}
+        symptom = int(request.form.get('symptoms'))
+        book = mydb["booked"]
+        #find the availability details
         for x in mycol.find():
             if x['username'] == username and x['date'] == date:
                 data = x
-                total = int(x['total'])
-                fromm = x['from']
-                to = x['to']
                 break
+        loong = svm.predict([[symptom]])
+        fromm = data['ltime']
+        to = data['to']
         ehr=int(to[0:2])
         emin=int(to[3:5])
-        if total==0:
-            shr=int(fromm[0:2])
-            smin=int(fromm[3:5])
-            time=add_time(15, shr, smin)
-            total+=1
-            newdata = data
-            newdata["total"] = total
-            newdata[str(total)] = {}
+        shr=int(fromm[0:2])
+        smin=int(fromm[3:5])
+        print(loong[0])
+        time=add_time(loong[0], shr, smin)
+        if cmp_time(int(time[0:2]), int(time[3:5]), ehr, emin): 
+            bookdata = { "dusername":username,"pusername": patient, "from":fromm, "to":time, "date":date}
+            book.insert_one(bookdata)
+            myquery = {"date" : date, "username": username}
+            newvalues = {"$set" : {"ltime" : time}}
+            mycol.update_one(myquery, newvalues)
             return render_template('appointments.html', dates=dates, message="Appointment booked for " + fromm )
+        else:
+            return render_template('appointments.html', dates=dates, message="Appointment not available")
     return render_template('appointments.html', dates=dates, message="")
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    return render_template('login.html')
 
 app.run(threaded=True)
